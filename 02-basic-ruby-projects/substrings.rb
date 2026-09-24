a47c27120c24d46d4271829d1f89e25258731de9
Vigneshkumar Chinnachamy M <thegardenerwhocodes@gmail.com>
2026-09-24 20:10:02 +0530

    implement substrings

--- 02-basic-ruby-projects/substrings.rb
+++ 02-basic-ruby-projects/substrings.rb
@@ -1,5 +1,15 @@
 module Substrings
   def self.substrings(word, dictionary)
-    return {}
+    out = {}
+    iword = word.downcase
+    len = iword.length
+
+    dictionary.each do |sub|
+      isub = sub.downcase
+      subs_len = len - iword.gsub(isub, '').length
+      num_subs = subs_len / sub.length
+      out[sub] = num_subs if num_subs > 0
+    end
+    return out
   end
 end
--- 02-basic-ruby-projects/test_substrings.rb
+++ 02-basic-ruby-projects/test_substrings.rb
@@ -3,11 +3,34 @@ require_relative 'substrings'
 
 class SubstringsTest < Minitest::Test
   def setup
-    @dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+    @dictionary = %w[ below down go going horn how howdy it i low own part partner
+                      sit ]
   end
 
   def test_substrings
-    assert_equal({"below" => 1, "low" => 1}, Substrings.substrings("below", @dictionary))
+    expected = {
+      'below' => 1,
+      'low' => 1
+    }
+    assert_equal expected, Substrings.substrings('below', @dictionary)
+  end
+
+  def test_substrings_multi_word
+    expected = {
+      'down' => 1,
+      'go' => 1,
+      'going' => 1,
+      'how' => 2,
+      'howdy' => 1,
+      'it' => 2,
+      'i' => 3,
+      'own' => 1,
+      'part' => 1,
+      'partner' => 1,
+      'sit' => 1
+    }
+
+    assert_equal expected,
+                 Substrings.substrings("Howdy partner, sit down! How's it going?", @dictionary)
   end
 end
-
